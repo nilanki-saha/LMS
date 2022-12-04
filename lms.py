6dccd3059ed2329ea9cfa63b596559543268ebc1
Nilanki Saha <114270905+nilanki-saha@users.noreply.github.com>
2022-12-04 22:22:41 +0530

    Update lms.py
    Library Management System using Python

--- lms.py
+++ lms.py
@@ -1,30 +1,36 @@
 import sys
 class Library:
-    def _init_(self, listofbooks):
+    def __init__(self, listofbooks):
         self.availablebooks = listofbooks
+        
     def displayAvailablebooks(self):
         print("The books we have in our library are as follows:")
         print("================================")
         for book in self.availablebooks:
             print(book)
+            
     def lendBook(self, requestedBook):
         if requestedBook in self.availablebooks:
             print("The book you requested has now been borrowed")
             self.availablebooks.remove(requestedBook)
         else:
             print("Sorry the book you have requested is currently not in the library")
+            
     def addBook(self, returnedBook):
         self.availablebooks.append(returnedBook)
         print("Thanks for returning your borrowed book")
+        
 class Student:
     def requestBook(self):
         print("Enter the name of the book you'd like to borrow>>")
         self.book=input()
         return self.book
+    
     def returnBook(self):
         print("Enter the name of the book you'd like to return>>")
         self.book=input()
         return self.book
+    
 def main():
     library=Library(["The Last Battle", "The Screwtape letters", "The Great Divorce"])
     student=Student()
